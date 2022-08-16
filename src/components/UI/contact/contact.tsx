8de7591af74db78b371bf3cecb357a6945cca150
Fran-Merce <franmerce1@gmail.com>
2022-08-15 21:44:25 -0300

    react-icons types

--- src/components/UI/contact/contact.tsx
+++ src/components/UI/contact/contact.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import { FiGithub,FiLinkedin } from 'react-icons/Fi';
-import {GoMail} from 'react-icons/Go';
-const iconStyle = 'text-cyan-500 opacity-[0.5] hover:opacity-100'
-
-
-
+import { FiGithub, FiLinkedin } from 'react-icons/Fi';
+import { GoMail } from 'react-icons/Go';
+const iconStyle = 'text-cyan-500 opacity-[0.5] hover:opacity-100';
 
 export const Contact = () => {
   return (
     <ul className='flex-col hidden  sm:flex fixed z-50 bottom-1 left-[3%] text-2xl gap-4'>
       <li>
         <a href='https://github.com/Fran-Merce' target={'_blank'}>
-          <FiGithub  className={iconStyle} />
+          <FiGithub className={iconStyle} />
         </a>
       </li>
       <li>
-        <a href='https://www.linkedin.com/in/francisco-merce/'  target={'_blank'}>
+        <a href='https://www.linkedin.com/in/francisco-merce/' target={'_blank'}>
           <FiLinkedin className={iconStyle} />
         </a>
       </li>
