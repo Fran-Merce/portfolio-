7da59832e61a3df4de9632f808c575a2ddf8710c
fran-merce <franmerce1@gmail.com>
2022-10-10 19:52:51 -0300

    fix export barrel

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { Contact, Navbar } from './components';
+import { Contact} from './components';
 import { Layout } from './components/Layout/Layout';
 import { AboutMe, Hero, Proyects } from './components/sections';
 import { useEffect } from 'react';
 import ParticlesComponent from './components/UI/Particles/Particles';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import { Skills } from './components/sections/Skills/Skills';
 import { Astronaut } from './components/UI/Astronaut';
 import { ContactSection } from './components/sections/Contact';
 import { ToastContainer } from 'react-toastify';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 const App = () => {
   useEffect(() => {
     AOS.init();
