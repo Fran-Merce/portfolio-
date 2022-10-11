f148915e34dc02b52dc8043503a45ba758df36c1
fran-merce <franmerce1@gmail.com>
2022-10-11 19:56:50 -0300

    restore

--- src/components/UI/Astronaut.tsx
+++ src/components/UI/Astronaut.tsx
@@ -5,32 +5,14 @@ import { Marker } from '../sections/AboutMe/Marker';
 export const Astronaut = () => {
   const [hovered, setHoverd] = useState<boolean>(false);
   const [clicked, setClicked] = useState<boolean>(false);
-  const [reprodusing, setReprodusing] = useState<boolean>(false);
   const refVideo = useRef<HTMLVideoElement>(null);
-  const [phrase, setPhrase] = useState({});
-  const randomPhrases = () => {
-    const numRand = Math.floor(Math.random() * dataAstronaut.length);
-    setPhrase(dataAstronaut[numRand]);
-  };
-  // Todo delete any type
-  const { phrase: phraseData, word1, word2, word3 } = phrase as any
-
   useEffect(() => {
-    if(reprodusing) return
-    if (refVideo.current) refVideo.current.volume = 0.7;
-    if (clicked && refVideo.current) {
-      randomPhrases()
-      refVideo.current.play();
-      setReprodusing(true);
-    }
-  }, [clicked]);
-
-  useEffect(() => {
-    if (clicked && !hovered) setTimeout(() => {
-      setClicked(false)
-      setReprodusing(false)
-    }, 6000);
+    if (clicked && !hovered) setTimeout(() => setClicked(false), 4500);
   }, [clicked, hovered]);
+  useEffect(() => {
+    if (refVideo.current) refVideo.current.volume = 0.7;
+    if (clicked && refVideo.current) refVideo.current.play();
+  }, [clicked]);
 
   return (
     <div className='bottom-0 fixed astronaut-animation  right-[40px]  w-fit sm:max-w-[100px]  '>
@@ -46,9 +28,9 @@ export const Astronaut = () => {
       <audio ref={refVideo} src='/audio-astronaut.mp3' />
       {clicked && (
         <p className='text-white text-xs  absolute min-w-[150px] left-0 top-[-80px] font-firaCode font-semibold  '>
-          {phraseData}
-          <Marker color='text-cyan-300'>{word1}</Marker>
-          <Marker color='text-red-700'>{word2}</Marker> {word3}
+          Do you know where I parked my {''}
+          <Marker color='text-cyan-300'>Rocket</Marker>?
+          <Marker color='text-red-700'>HEY!!</Marker> you speak english?
         </p>
       )}
     </div>
