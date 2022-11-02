72667e9767f2d2f5ab26fcce735bff380c0f95b3
fran-merce <franmerce1@gmail.com>
2022-11-02 12:03:39 -0300

    github link on projects section replaced for anchor tag

--- src/components/sections/Projects/ProjectsSection/Projects.tsx
+++ src/components/sections/Projects/ProjectsSection/Projects.tsx
@@ -17,10 +17,9 @@ const Projects = () => {
         <p className='text-xs sm:text-sm text-white w-full'>
           Puedes ver mas codigo y bugs en mi github
         </p>
-        <FaGithub
-          onClick={() => window.open('https://github.com/Fran-Merce', '_blank')}
-          className='text-center block  m-auto my-4 text-2xl text-cyan-600 hover:text-cyan-500 cursor-pointer hover:scale-125 transition-all'
-        />
+        <a href='https://github.com/Fran-Merce' className='w-fit' target='_blanck'>
+          <FaGithub className='text-center block  m-auto my-4 text-2xl text-cyan-600 hover:text-cyan-500 cursor-pointer hover:scale-125 transition-all' />
+        </a>
       </div>
     </section>
   );
