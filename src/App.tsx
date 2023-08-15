b80b8102bb978c1e507710cc3d7a4f6b20c779c0
Fran-Merce <franmerce11@gmail.com>
2023-08-15 11:44:06 -0300

    analitycs

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,21 @@ import { Skills } from './components/sections/Skills/Skills';
 import { Astronaut } from './components/UI/Astronaut';
 import { ContactSection } from './components/sections/Contact';
 import { ContactLeft } from './components';
+import { useEffect } from 'react';
+import ReactGA from "react-ga4";
 
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize("G-MNZHJ642P1");
+    ReactGA.event({
+      category: "your category",
+      action: "your action",
+      label: "your label", // optional
+      value: 99, // optional, must be a number
+      nonInteraction: true, // optional, true
+    });
+  },[])
   return (
     <Layout>
       <main>
